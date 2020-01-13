44caf184f51ce92777b4bf48f342dd3f2815e6a4
Preston-Shipman <pshipman2000@gmail.com>
2020-01-13 15:58:45 -0800

    Work in progress

--- server/index.js
+++ server/index.js
@@ -1,21 +1,35 @@
 require('dotenv/config');
 const express = require('express');
-
 const db = require('./database');
 const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
-
 const app = express();
 
 app.use(staticMiddleware);
 app.use(sessionMiddleware);
-
 app.use(express.json());
 
 app.get('/api/health-check', (req, res, next) => {
-  db.query('select \'successfully connected\' as "message"')
+  db.query("select 'successfully connected' as \"message\"")
     .then(result => res.json(result.rows[0]))
+    .catch(err => next(err));
+});
+
+app.get('/api/products', (req, res, next) => {
+  const sql = `
+    select "productId",
+    "name",
+    "price",
+    "image",
+    "shortDescription"
+      from "products"
+  `;
+  db.query(sql)
+    .then(result => {
+      const product = result.rows;
+      res.json(product);
+    })
     .catch(err => next(err));
 });
 
