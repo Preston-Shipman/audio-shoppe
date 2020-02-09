031ca4f556ebdfc85d2828f6a12a8201c22a9938
Preston-Shipman <pshipman2000@gmail.com>
2020-02-08 16:53:23 -0800

    Work in progress

---
+++ client/components/cart-summary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function CartSummaryItem(props) {
+  return render(
+    this.props.image,
+    this.props.name,
+    this.props.price,
+    this.props.shortDescription
+  );
+}
