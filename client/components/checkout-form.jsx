38529f110685125892d6b6502a303fb703ca57f5
Preston-Shipman <pshipman2000@gmail.com>
2020-02-17 19:52:43 -0800

    Work in progress finished validation

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -16,13 +16,14 @@ class CheckoutForm extends React.Component {
         }
       }
     };
-    this.setView = this.setView.bind(this);
+    this.inputHandler = this.inputHandler.bind(this);
   }
 
   inputHandler(event) {
     const nameOfTarget = event.target.name;
     const value = event.target.value;
     const creditCardRegEx = /^[0-9]{13,16}$/;
+    const creditInput = document.getElementById('cc');
     let isValid = false;
 
     this.setState({
@@ -34,38 +35,71 @@ class CheckoutForm extends React.Component {
         }
       }
     });
-    if (this.state.form.creditCard.value === creditCardRegEx) {
-      isValid = true;
-    }
-    if (isValid === true) {
-      alert('Thank You');
-    } else {
-      alert('Please provide a valid credit card number');
-    }
+
+    creditInput.onblur = e => {
+      if (this.state.form.creditCard.value !== '') {
+        if (creditCardRegEx.test(this.state.form.creditCard.value) === true) {
+          isValid = true;
+        }
+        if (isValid === true) {
+          alert('Thank You');
+        } else {
+          alert('Please provide a valid credit card number');
+        }
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('Nothing');
+      }
+    };
+
   }
 
   render() {
     return (
-      <form>
-        <input
-          type="text"
-          name="customerName"
-          value={this.state.form.customerName.value}
-          onChange={this.inputHandler}
-        />
-        <input
-          type="text"
-          name="creditCard"
-          value={this.state.form.creditCard.value}
-          onChange={this.inputHandler}
-        />
-        <input
-          type="text"
-          name="shippingAddress"
-          value={this.state.form.shippingAddress.value}
-          onChange={this.inputHandler}
-        />
-      </form>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <form>
+              <div className="form-group">
+                <label htmlFor="name"> Name </label>
+                <input
+                  className="form-control"
+                  id="name"
+                  type="text"
+                  name="customerName"
+                  value={this.state.form.customerName.value}
+                  onChange={this.inputHandler}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="cc"> Credit Card </label>
+                <input
+                  className="form-control"
+                  id="cc"
+                  type="text"
+                  name="creditCard"
+                  value={this.state.form.creditCard.value}
+                  onChange={this.inputHandler}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="shippingInfo"> Shipping Address </label>
+                <input
+                  className="form-control"
+                  id="sBlurippingInfo"
+                  type="text"
+                  name="shippingAddress"
+                  value={this.state.form.shippingAddress.value}
+                  onChange={this.inputHandler}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     );
   }
 }
